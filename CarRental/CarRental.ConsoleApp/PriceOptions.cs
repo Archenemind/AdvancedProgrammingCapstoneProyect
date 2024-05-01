992e2cc5252a42a1ae9c86f18f7e66d04c895090
Carlos Garcia <qesloqpongo@gmail.com>
2024-05-01 18:36:43 -0400

    Created price menu and main menu

---
+++ CarRental/CarRental.ConsoleApp/PriceOptions.cs
@@ -0,0 +1,115 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarRental.Domain.Entities.Types;
+using Grpc.Net.Client;
+using System.Net;
+using CarRental.grpc;
+using System.Threading.Channels;
+
+namespace CarRental.ConsoleApp
+{
+    internal partial class Program
+    {
+        internal static void PriceMenu(CarRental.grpc.Price.PriceClient client)
+        {
+            PriceDTO? createResponse = null;
+            string? SelectedOption = null;
+            while (SelectedOption != "5")
+            {
+                Console.Clear();
+
+                Console.WriteLine("Price Menu:\n\n");
+                Console.WriteLine("Press 1 for creating a price");
+                Console.WriteLine("Press 2 for getting a price");
+                Console.WriteLine("Press 3 for updating a price");
+                Console.WriteLine("Press 4 deleting a price");
+                Console.WriteLine("Press 5 to go back");
+
+                SelectedOption = Convert.ToString(Console.ReadLine());
+
+                switch (SelectedOption)
+                {
+                    case "1":
+                        createResponse = client.CreatePrice(new CreatePriceRequest() { Value = 5, MoneyType = MoneyTypes.Mlc });
+                        if (createResponse is null)
+                        {
+                            Console.WriteLine("Cannot create price");
+                            return;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\bCreated new price.\b");
+                            Console.WriteLine("\nPress a new key to continue");
+                            Console.ReadKey();
+                        }
+                        break;
+
+                    case "2":
+                        var getResponse = client.GetPrice(new GetRequest() { Id = 1 });
+                        if (getResponse.Price is null)
+                        {
+                            Console.WriteLine("Cannot get price");
+                            Console.WriteLine("\nPress a new key to continue");
+                            Console.ReadKey();
+                            return;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Price obtained {getResponse.Price.Value} {getResponse.Price.MoneyType.ToString()}");
+                            Console.WriteLine("\nPress a new key to continue");
+                            Console.ReadKey();
+                        }
+                        break;
+
+                    case "3":
+                        createResponse.Value = 20;
+                        client.UpdatePrice(createResponse);
+
+                        var updatedGetResponse = client.GetPrice(new GetRequest() { Id = createResponse.Id });
+                        if (updatedGetResponse is not null && updatedGetResponse.KindCase == NullablePriceDTO.KindOneofCase.Price && updatedGetResponse.Price.Value == 20)
+                        {
+                            Console.WriteLine("Successfully modified");
+                            Console.WriteLine("\nPress a new key to continue");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Could not update :(");
+                            Console.WriteLine("\nPress a new key to continue");
+                            Console.ReadKey();
+                        }
+                        break;
+
+                    case "4":
+                        client.DeletePrice(createResponse);
+                        var deletedGetResponse = client.GetPrice(new GetRequest() { Id = createResponse.Id });
+                        if (deletedGetResponse is null || deletedGetResponse.KindCase != NullablePriceDTO.KindOneofCase.Price)
+                        {
+                            Console.WriteLine("Successfully deleted");
+                            Console.WriteLine("\nPress a new key to continue");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Could not delete");
+                            Console.WriteLine("\nPress a new key to continue");
+                            Console.ReadKey();
+                        }
+
+                        break;
+
+                    case "5":
+                        break;
+
+                    default:
+                        Console.Write("\nWrong key");
+                        break;
+                }
+            }
+        }
+    }
+}
--- CarRental/CarRental.ConsoleApp/Program.cs
+++ CarRental/CarRental.ConsoleApp/Program.cs
@@ -2,20 +2,17 @@
 using Grpc.Net.Client;
 using System.Net;
 using CarRental.grpc;
+using System.Threading.Channels;
 
 namespace CarRental.ConsoleApp
 {
-    internal class Program
+    internal partial class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Presione una tecla para conectar");
-            Console.ReadKey();
-
-            Console.WriteLine("Creating channel and client");
             var httpHandler = new HttpClientHandler();
             httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
-            var channel = GrpcChannel.ForAddress("http://localhost:5051", new GrpcChannelOptions { HttpHandler = httpHandler });
+            GrpcChannel channel = GrpcChannel.ForAddress("http://localhost:5051", new GrpcChannelOptions { HttpHandler = httpHandler });
             if (channel is null)
             {
                 Console.WriteLine("Cannot connect");
@@ -23,58 +20,38 @@ namespace CarRental.ConsoleApp
                 return;
             }
 
-            var client = new CarRental.grpc.Price.PriceClient(channel);
-
-            Console.WriteLine("Presione una tecla para crear un precio");
-            Console.ReadKey();
-            var createResponse = client.CreatePrice(new CreatePriceRequest() { Value = 5, MoneyType = MoneyTypes.Mlc });
-            if (createResponse is null)
+            while (true)
             {
-                Console.WriteLine("Cannot create price");
-                channel.Dispose();
-                return;
-            }
-            else
-            {
-                Console.WriteLine($"Creación exitosa.");
-            }
+                Console.Clear();
 
-            Console.WriteLine("Presione una tecla para obtener el precio");
-            Console.ReadKey();
-            var getResponse = client.GetPrice(new GetRequest() { Id = 1 });
-            if (getResponse.Price is null)
-            {
-                Console.WriteLine("Cannot get price");
-                channel.Dispose();
-                return;
+                Console.WriteLine("Menu:\n\n\n");
+                Console.WriteLine("Press 1 for price options");
+                Console.WriteLine("Press 2 for vehicle options");
+                Console.WriteLine("Press 3 for person options");
+                Console.WriteLine("Press 4 for reservation options");
+                Console.WriteLine("Press 5 to exit");
+
+                string? option = Convert.ToString(Console.ReadLine());
+
+                switch (option)
+                {
+                    case "1":
+                        CarRental.grpc.Price.PriceClient client = new CarRental.grpc.Price.PriceClient(channel);
+                        PriceMenu(client);
+                        break;
+
+                    case "5":
+                        Console.WriteLine("Goodbye :(");
+                        channel.Dispose();
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("\nWrong key");
+
+                        break;
+                }
             }
-            else
-            {
-                Console.WriteLine($"Obtención exitosa {getResponse.Price.Value} {getResponse.Price.MoneyType.ToString()}");
-            }
-
-            Console.WriteLine("Presione una tecla para modificar el precio");
-            Console.ReadKey();
-            createResponse.Value = 20;
-            client.UpdatePrice(createResponse);
-
-            var updatedGetResponse = client.GetPrice(new GetRequest() { Id = createResponse.Id });
-            if (updatedGetResponse is not null && updatedGetResponse.KindCase == NullablePriceDTO.KindOneofCase.Price && updatedGetResponse.Price.Value == 20)
-            {
-                Console.WriteLine($"Modificación exitosa.");
-            }
-
-            Console.WriteLine("Presione una tecla para eliminar el precio");
-            Console.ReadKey();
-
-            client.DeletePrice(createResponse);
-            var deletedGetResponse = client.GetPrice(new GetRequest() { Id = createResponse.Id });
-            if (deletedGetResponse is null || deletedGetResponse.KindCase != NullablePriceDTO.KindOneofCase.Price)
-            {
-                Console.WriteLine($"Eliminación exitosa.");
-            }
-
-            channel.Dispose();
         }
     }
 }
